7222f8eb316515bf6a31e349b5d077a31de1a1fa
sunqi <sunqi_925@163.com>
2016-09-29 22:25:09 +0800

    333

--- upload.php
+++ upload.php
@@ -1,30 +1,41 @@
 <?php
-$UPLOAD_FILE_STORAGE_ROOT = __DIR__.'/upload';
-$UPLOAD_FILE_MAX_SIZE = 2*1024*1024;
-$UPLOAD_FILE_ALLOW_TYPES = ['image/jpg','image/jpeg','image/png'];
+//configuration
+$UPLOAD_FILE_STORAGE_ROOT = __DIR__; //文件保存根目录
+$UPLOAD_FILE_STORAGE_DIR = 'upload'; //保存所有上传文件的目录名
+$UPLOAD_FILE_MAX_SIZE = 2*1024*1024;  //上传文件最大2M
+$UPLOAD_FILE_ALLOW_TYPES = ['image/jpg','image/jpeg','image/png']; //允许文件类型
 
+//统一出错处理
+function errorResponse($errorMsg){
+    echo json_encode(['success'=>false,'error'=>$errorMsg]);
+}
+
+
+if (!isset($_FILES['file'])) {
+    errorResponse('未上传文件');
+}
 if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-     $type = $_FILES['file']['type'];
-     $size = $_FILES['file']['size'];
-     if(!in_array($type,$UPLOAD_FILE_ALLOW_TYPES)){
-        echo json_encode(['success'=>false,'error'=>'文件类型不正确']);
-        exit;
+    $type = $_FILES['file']['type'];
+    $size = $_FILES['file']['size'];
+    if(!in_array($type,$UPLOAD_FILE_ALLOW_TYPES)){
+        errorResponse('文件类型不正确');
+    }
+    if ($size > $UPLOAD_FILE_MAX_SIZE) {
+        errorResponse('文件过大，最大2M');
      }
-     if ($size > $UPLOAD_FILE_MAX_SIZE) {
-        echo json_encode(['success'=>false,'error'=>'文件过大,最大支持2M']);
-        exit;
-     }
-     $ext = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
-     $storePath =$UPLOAD_FILE_STORAGE_ROOT.DIRECTORY_SEPARATOR.date('Y-m-d');
-     if (!is_dir($storePath)) {
+    $ext = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
+    $filePath = $UPLOAD_FILE_STORAGE_DIR.'/'.date('Y-m-d');
+    $storePath =$UPLOAD_FILE_STORAGE_ROOT.DIRECTORY_SEPARATOR.$filePath;
+    if (!is_dir($storePath)) {
          mkdir($storePath,true);
-     }
-     $filename = md5(time().str_pad(mt_rand(0,99999),5,'0')).'.'.$ext;
-     $destination = $storePath.DIRECTORY_SEPARATOR.$filename;
-     $moveResult = move_uploaded_file($_FILES['file']['tmp_name'],$destination);
-     if ($moveResult) {
-        echo json_encode(['success'=>true,'data'=>['file_url'=>$destination]]);
-     } else {
-        echo json_encode(['success'=>false,'error'=>'服务器保存文件出错']);
-     }
+    }
+    $filename = md5(time().str_pad(mt_rand(0,99999),5,'0')).'.'.$ext;
+    $destination = $storePath.DIRECTORY_SEPARATOR.$filename;
+    $moveResult = move_uploaded_file($_FILES['file']['tmp_name'],$destination);
+    if ($moveResult) {
+        echo json_encode(['success'=>true,
+            'data'=>['file_url'=>'/'.$filePath.'/'.$filename]]);
+    } else {
+        errorResponse('服务器保存文件出错');
+    }
 }
