d48ba7d621c6c0b754291bd22b83efb386db7023
sunqisteven <sunqi_925@163.com>
2017-11-20 05:23:02 -0600

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -1,13 +1,65 @@
 <?php
 //connect to database by using PDO
-$dsn = 'mysql:dbname=app;host=127.0.0.1';
-$user= 'root';
-$pwd = '';
-$pdo = new PDO($dsn,$user,$pwd);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-//解决取出的ID是字符的bug
-$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
-$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$stmt = $pdo->query('select * from account');
-$accounts = $stmt->fetch();
-// var_dump($accounts['id']);	
+class Db_service {
+	public function __construct(){
+		$dsn = 'mysql:dbname=app;host=127.0.0.1';
+		$user= 'root';
+		$pwd = 'root';
+		try{
+			$pdo = new PDO($dsn,$user,$pwd);
+			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+			//解决取出的ID是字符的bug
+			$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+			$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+			$this->pdo = $pdo;
+		}catch(Exception $e){
+			exit($e->getMessage());
+		}
+	}
+	public static function getInstance(){
+		return (new Db_service());
+	}
+	public function query($sql,$params){
+		$stmt = $this->pdo->prepare($sql);
+		$result = $stmt->execute($params);
+		if ($result) {
+			return $stmt->fetch();
+		} else {
+			return false;
+		}
+	}
+	public function execute($sql,$params){
+		try{
+			$stmt = $this->pdo->prepare($sql);
+			$result = $stmt->execute($params);
+			// var_dump($stmt->errorinfo());
+			// return (int)$this->pdo->lastInsertId();
+			return $result;
+		}catch(Exception $e){
+			return false;
+		}
+		
+	}
+	public function insertGetId($sql,$params){
+		try{
+			$stmt = $this->pdo->prepare($sql);
+			$result = $stmt->execute($params);
+			// var_dump($stmt->errorinfo());
+			return (int)$this->pdo->lastInsertId();
+			// return $result;
+		}catch(Exception $e){
+			return false;
+		}
+	}
+}
+
+// $r = Db_service::getInstance()->execute('update account set username=? where id=?',['22',2]);
+// var_dump($r);
+
+
+
+
+
+
+
+
