0f6a53bc7742de4385f6845194905999570d53de
sunqisteven <sunqi_925@163.com>
2017-11-20 05:28:20 -0600

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -1,20 +1,19 @@
 <?php
 //connect to database by using PDO
+set_exception_handler(function($e){
+	echo $e->getMessage();
+});
 class Db_service {
 	public function __construct(){
 		$dsn = 'mysql:dbname=app;host=127.0.0.1';
 		$user= 'root';
-		$pwd = 'root';
-		try{
-			$pdo = new PDO($dsn,$user,$pwd);
-			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+		$pwd = 'roo';
+		$pdo = new PDO($dsn,$user,$pwd);
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 			//解决取出的ID是字符的bug
-			$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
-			$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-			$this->pdo = $pdo;
-		}catch(Exception $e){
-			exit($e->getMessage());
-		}
+		$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$this->pdo = $pdo;
 	}
 	public static function getInstance(){
 		return (new Db_service());
@@ -29,32 +28,18 @@ class Db_service {
 		}
 	}
 	public function execute($sql,$params){
-		try{
 			$stmt = $this->pdo->prepare($sql);
 			$result = $stmt->execute($params);
-			// var_dump($stmt->errorinfo());
-			// return (int)$this->pdo->lastInsertId();
 			return $result;
-		}catch(Exception $e){
-			return false;
-		}
 		
 	}
 	public function insertGetId($sql,$params){
-		try{
 			$stmt = $this->pdo->prepare($sql);
 			$result = $stmt->execute($params);
-			// var_dump($stmt->errorinfo());
 			return (int)$this->pdo->lastInsertId();
-			// return $result;
-		}catch(Exception $e){
-			return false;
-		}
 	}
 }
 
-// $r = Db_service::getInstance()->execute('update account set username=? where id=?',['22',2]);
-// var_dump($r);
 
 
 
